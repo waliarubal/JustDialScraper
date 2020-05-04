5208f2faab165f0cf4c03e9f28197f0cf5b0eba5
Rubal Walia <walia.rubal@gmail.com>
2020-05-04 21:08:18 +0530

    Added common project.

---
+++ src/JustDialScraper.Common/Base/ViewModelBase.cs
@@ -0,0 +1,34 @@
+﻿namespace JustDialScraper.Common.Base
+{
+    public abstract class ViewModelBase : ModelBase
+    {
+        protected ViewModelBase()
+        {
+
+        }
+
+        #region properties
+
+        public abstract bool IsCachable { get; }
+
+        public string Title
+        {
+            get => Get<string>();
+            protected set => Set(value);
+        }
+
+        public bool IsBusy
+        {
+            get => Get<bool>();
+            protected set => Set(value);
+        }
+
+        public bool IsLoaded
+        {
+            get => Get<bool>();
+            internal set => Set(value);
+        }
+
+        #endregion
+    }
+}
---
+++ src/JustDialScraper.Common/Commands/RelayCommand.cs
@@ -0,0 +1,66 @@
+﻿using JustDialScraper.Common.Base;
+using System;
+using System.Threading.Tasks;
+
+namespace JustDialScraper.Common.Commands
+{
+    public class RelayCommand : CommandBase
+    {
+        readonly Action _action;
+
+        public RelayCommand(Action action, bool isEnabled = true, bool isAsynchronous = false) : base(isEnabled, isAsynchronous)
+        {
+            _action = action;
+        }
+
+        public async override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            IsExecuting = true;
+
+            if (IsAsynchronous)
+                await Task.Run(_action);
+            else
+                _action.Invoke();
+
+            IsExecuting = false;
+        }
+    }
+
+    public class RelayCommand<P> : CommandBase
+    {
+        readonly Action<P> _action;
+
+        public RelayCommand(Action<P> action, bool isEnabled = true, bool isAsynchronous = false) : base(isEnabled, isAsynchronous)
+        {
+            _action = action;
+        }
+
+        public async override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            IsExecuting = true;
+
+            P argument;
+            try
+            {
+                argument = (P)parameter;
+            }
+            catch (InvalidCastException)
+            {
+                argument = default;
+            }
+
+            if (IsAsynchronous)
+                await Task.Run(() => _action.Invoke(argument));
+            else
+                _action.Invoke(argument);
+
+            IsExecuting = false;
+        }
+    }
+}
