e2dc8fd8a48592fd2d9f875c9d2aa2d09086ef7f
Rubal Walia <walia.rubal@gmail.com>
2020-05-05 18:46:49 +0530

    UI changes for main view.

---
+++ src/JustDialScraper.Ui/Models/ScanOptionsModel.cs
@@ -0,0 +1,8 @@
+﻿using JustDialScraper.Common.Base;
+
+namespace JustDialScraper.Ui.Models
+{
+    public class ScanOptionsModel: ModelBase
+    {
+    }
+}
---
+++ src/JustDialScraper.Ui/Services/JustDialService/JustDialService.cs
@@ -0,0 +1,6 @@
+﻿namespace JustDialScraper.Ui.Services
+{
+    public class JustDialService: IJustDialService
+    {
+    }
+}
