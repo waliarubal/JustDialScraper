710367cbb8896f48b2a861535004d570c09a0a50
Rubal Walia <walia.rubal@gmail.com>
2020-05-10 22:34:56 +0530

    Using PuppeteerSharp.

--- src/JustDialScraper.Ui/Services/JustDial/JustDialService.cs
+++ src/JustDialScraper.Ui/Services/JustDial/JustDialService.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using PuppeteerSharp;
+using PuppeteerSharp.Input;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Zu.AsyncWebDriver.Remote;
-using Zu.Chrome;
-using Zu.WebBrowser.BasicTypes;
 
 namespace JustDialScraper.Ui.Services
 {
@@ -12,69 +10,80 @@ namespace JustDialScraper.Ui.Services
     {
         const string JUST_DIAL_URL = "https://www.justdial.com/";
 
-        WebDriver _driver;
-
-        WebDriver Driver
-        {
-            get
-            {
-                if (_driver == null)
-                {
-                    var isHeadless = false; //!Debugger.IsAttached;
-
-                    var config = new ChromeDriverConfig();
-                    config.SetCommandLineArgumets("--disable-gpu --window-size=1280,800");
-                    config.Headless = isHeadless;
-
-                    var chromeDriver = new AsyncChromeDriver(config);
-
-                    _driver = new WebDriver(chromeDriver);
-                }
-
-                return _driver;
-            }
-        }
-
-        TimeSpan WaitTime { get; } = TimeSpan.FromSeconds(10);
-
         public void Dispose()
         {
-            if (_driver == null)
-                return;
-
-            Driver.Close();
-            Driver.Dispose();
+            
         }
 
         public async Task<IEnumerable<string>> GetLocations(string keyword)
         {
             var locations = new List<string>();
 
-            await Driver.GoToUrl(JUST_DIAL_URL);
+            var browserFetcher = new BrowserFetcher();
+            await browserFetcher.DownloadAsync(BrowserFetcher.DefaultRevision);
 
-            var cityInputNode = await Driver.FindElementById("city", WaitTime);
-            if (cityInputNode == null)
-                return locations;
-
-            await cityInputNode.Click();
-            await cityInputNode.Clear();
-            if (!string.IsNullOrWhiteSpace(keyword))
-                await cityInputNode.SendKeys(keyword);   
-            await cityInputNode.SendKeys(Keys.Return);
-
-            var cityDropDownNode = await Driver.FindElementByCssSelector("span[class=\"city-drop dn\"]", WaitTime);
-            if (cityDropDownNode == null)
-                return locations;
-
-            var cityNodes = await Driver.FindElementsByCssSelector("span[class=\"city-drop dn\"]>ul>li", WaitTime);
-            if (cityNodes == null)
-                return locations;
-
-            foreach (var cityNode in cityNodes)
+            var browserOptions = new LaunchOptions
             {
-                var cityName = await cityNode.GetAttribute("id");
+                Headless = false,
+                IgnoreHTTPSErrors = true,
+                Timeout = 30000
+            };
+
+            var browser = await Puppeteer.LaunchAsync(browserOptions);
+
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync(JUST_DIAL_URL);
+
+            var cityInput = await page.WaitForSelectorAsync("#city");
+            if (cityInput == null)
+                return locations;
+
+            await cityInput.FocusAsync();
+            await cityInput.TypeAsync(string.Empty);
+            await page.Keyboard.PressAsync(Key.Enter);
+
+            var citiesDropdown = await page.WaitForSelectorAsync("span[class=\"city-drop dn\"]");
+            if (citiesDropdown == null)
+                return locations;
+
+            var cityNodes = await page.QuerySelectorAllAsync("span[class=\"city-drop dn\"]>ul>li");
+            foreach(var cityNode in cityNodes)
+            {
+                var idProperty = await cityNode.GetPropertyAsync("id");
+                var cityName = idProperty.RemoteObject.Value.ToString();
+
                 locations.Add(cityName);
             }
+
+            await page.DisposeAsync();
+
+            await browser.DisposeAsync();
+
+            //await Driver.GoToUrl(JUST_DIAL_URL);
+
+            //var cityInputNode = await Driver.FindElementById("city", WaitTime);
+            //if (cityInputNode == null)
+            //    return locations;
+
+            //await cityInputNode.Click();
+            //await cityInputNode.Clear();
+            //if (!string.IsNullOrWhiteSpace(keyword))
+            //    await cityInputNode.SendKeys(keyword);   
+            //await cityInputNode.SendKeys(Keys.Return);
+
+            //var cityDropDownNode = await Driver.FindElementByCssSelector("span[class=\"city-drop dn\"]", WaitTime);
+            //if (cityDropDownNode == null)
+            //    return locations;
+
+            //var cityNodes = await Driver.FindElementsByCssSelector("span[class=\"city-drop dn\"]>ul>li", WaitTime);
+            //if (cityNodes == null)
+            //    return locations;
+
+            //foreach (var cityNode in cityNodes)
+            //{
+            //    var cityName = await cityNode.GetAttribute("id");
+            //    locations.Add(cityName);
+            //}
 
             return locations;
         }
